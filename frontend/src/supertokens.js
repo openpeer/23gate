1eaf5e9239114a80ea8b82d1fff5c2d2a8e3619a
Marcos Teixeira <m.viniteixeira@gmail.com>
2024-02-09 19:42:53 +0000

    Update dockerfile

--- frontend/src/supertokens.js
+++ frontend/src/supertokens.js
@@ -1,7 +1,7 @@
-import SuperTokens from 'supertokens-web-js';
-import EmailPasswordReact from 'supertokens-auth-react/recipe/emailpassword';
-import SessionReact from 'supertokens-auth-react/recipe/session';
-import Session from 'supertokens-web-js/recipe/session';
+import SuperTokens from 'supertokens-web-js'
+import EmailPasswordReact from 'supertokens-auth-react/recipe/emailpassword'
+import SessionReact from 'supertokens-auth-react/recipe/session'
+import Session from 'supertokens-web-js/recipe/session'
 
 export const SuperTokensReactConfig = {
   appInfo: {
@@ -9,24 +9,22 @@ export const SuperTokensReactConfig = {
     apiDomain: import.meta.env.VITE_SUPERTOKENS_APIDOMAIN,
     websiteDomain: import.meta.env.VITE_SUPERTOKENS_WEBSITEDOMAIN,
     apiBasePath: '/auth',
-    websiteBasePath: '/a'
+    websiteBasePath: '/a',
   },
   recipeList: [
     EmailPasswordReact.init({
-      useShadowDom: false
+      useShadowDom: false,
     }),
-    SessionReact.init()
-  ]
-};
+    SessionReact.init(),
+  ],
+}
 
 export const SuperTokensWebJSConfig = {
   appInfo: {
     appName: import.meta.env.VITE_SUPERTOKENS_APPNAME,
-    apiDomain: import.meta.env.VITE_SUPERTOKENS_APIDOMAIN
+    apiDomain: import.meta.env.VITE_SUPERTOKENS_APIDOMAIN,
   },
-  recipeList: [
-    Session.init()
-  ]
-};
-
-SuperTokens.init(SuperTokensWebJSConfig);
+  recipeList: [Session.init()],
+}
+console.log('SuperTokensWebJSConfig', SuperTokensWebJSConfig)
+SuperTokens.init(SuperTokensWebJSConfig)
