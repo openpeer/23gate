6fd3a00ebd753e96cfcff280a1d2daf254b6d3bc
Marcos Teixeira <m.viniteixeira@gmail.com>
2024-02-13 16:53:32 +0000

    Make transactionHash search case insensitive

--- backend/models/Transaction.model.mjs
+++ backend/models/Transaction.model.mjs
@@ -1,10 +1,10 @@
-import { Sequelize, Model } from 'sequelize';
+import { Sequelize, Model } from 'sequelize'
 
 export class Transaction extends Model {
   static structure = {
     chainId: {
       type: Sequelize.DataTypes.BIGINT.UNSIGNED,
-      allowNull: false
+      allowNull: false,
       // chainId is not part of the primary key because we fully expect tx hash to be unique enough.
     },
 
@@ -13,65 +13,71 @@ export class Transaction extends Model {
       allowNull: false,
       primaryKey: true,
       get() {
-        return '0x' + this.getDataValue('transactionHash').toString('hex');
-      }
+        return '0x' + this.getDataValue('transactionHash').toString('hex')
+      },
     },
 
     blockNumber: {
       type: Sequelize.DataTypes.BIGINT.UNSIGNED,
-      allowNull: false
+      allowNull: false,
     },
 
     transactionIndex: {
       type: Sequelize.DataTypes.SMALLINT.UNSIGNED,
-      allowNull: false
+      allowNull: false,
     },
 
     json: {
       type: Sequelize.DataTypes.JSON,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   }
 
   static async destroyTransactionsByHashList(transactionHash, transaction) {
     await this.destroy({
-      where: {
-        transactionHash
-      },
-      transaction
-    });
+      where: Sequelize.where(
+        Sequelize.fn('lower', Sequelize.col('transactionHash')),
+        Sequelize.fn('lower', transactionHash)
+      ),
+      transaction,
+    })
   }
 
   static async findTransactionsToParseByChainId(chainId, limit = 30) {
     return await this.findAll({
       where: {
-        chainId
+        chainId,
       },
-      order: [ // we must sort in order to put sorted into Payload table as they are delivered sequentially
+      order: [
+        // we must sort in order to put sorted into Payload table as they are delivered sequentially
         ['blockNumber', 'asc'],
         ['transactionIndex', 'asc'],
       ],
-      limit
-    });
+      limit,
+    })
   }
 
   static async ingestReceiptsForChainId(receipts, chainId, transaction) {
-    const records = receipts.map(receipt => (
-      '(' +
+    const records = receipts.map(
+      (receipt) =>
+        '(' +
         [
           this.sequelize.escape(chainId),
           receipt.transactionHash,
           this.sequelize.escape(receipt.blockNumber),
           this.sequelize.escape(receipt.transactionIndex),
-          this.sequelize.escape(JSON.stringify(receipt))
+          this.sequelize.escape(JSON.stringify(receipt)),
         ].join(', ') +
-      ')'
-    ));
+        ')'
+    )
 
-    await this.sequelize.query(`INSERT IGNORE INTO Transaction (chainId, transactionHash, blockNumber, transactionIndex, json) VALUES ${records}`, {
-      transaction,
-      type: Sequelize.QueryTypes.UPDATE
-    });
+    await this.sequelize.query(
+      `INSERT IGNORE INTO Transaction (chainId, transactionHash, blockNumber, transactionIndex, json) VALUES ${records}`,
+      {
+        transaction,
+        type: Sequelize.QueryTypes.UPDATE,
+      }
+    )
   }
 
   static options = {
@@ -79,14 +85,14 @@ export class Transaction extends Model {
 
     indexes: [
       {
-        fields: ['chainId']
+        fields: ['chainId'],
       },
       {
-        fields: ['blockNumber', 'transactionIndex']
+        fields: ['blockNumber', 'transactionIndex'],
       },
       {
-        fields: ['chainId', 'blockNumber', 'transactionIndex']
-      }
-    ]
+        fields: ['chainId', 'blockNumber', 'transactionIndex'],
+      },
+    ],
   }
 }
